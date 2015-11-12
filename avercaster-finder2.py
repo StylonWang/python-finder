e915fa4c8ca9231c80400263909489a6a8652de7
Stylon Wang <wangstylon@gmail.com>
2015-11-12 14:51:35 +0800

    finally have a working finder for F239/F239+/F236.

--- avercaster-finder2.py
+++ avercaster-finder2.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python
-#
 # This tool searches for AVerCaster series product in the local LAN,
-# by listening to SSDP NOTIFY messages. This only works on F239/F239+, not F236.
+# by sending SSDP requests and receive replies.
 #
-# Known issues: if not enough SSDP NOTIFY messages are received,
-#               first while loop will not end because sock.recvfrom is blocking.
-#
+# Works for F239, F239+ and F236.
 
 import socket
 import struct
@@ -13,39 +10,49 @@ import re
 import time
 import urllib2, urllib
 import xml.etree.ElementTree as ET
+import select
 
 MCAST_GRP = '239.255.255.250'
 MCAST_PORT = 1900
+MSG = "M-SEARCH * HTTP/1.1\r\nST: urn:schemas-avermedia-com:avercaster:1\r\nMX: 10\r\nMAN: \"ssdp:discover\"\r\nHOST: 239.255.255.250:1900\r\n\r\n"
+TIME_OUT = 2
 
-WORK_TIME = 5 # default to collect SSDP for 10 seconds
-
-# set up to receive SSDP multicast messages
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.bind((MCAST_GRP, MCAST_PORT))
-mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
- 
-sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
+sock.setblocking(0)
 
 avercasters = {}
 start_time = time.time()
- 
-# receive SSDP messages
-while True:
-    #print sock.recv(1024)
-    msg = sock.recvfrom(1024)
-    if not re.match("^NOTIFY", msg[0]) :
-        continue # not a SSDP NOTIFY message, skip
 
-    if not re.search("NT: urn:schemas-avermedia-com:avercaster:1", msg[0]) :
+# send M-SEARCH
+sock.sendto(MSG, (MCAST_GRP, MCAST_PORT))
+
+# receive response
+while True:
+
+    # implement non-blocking so we can timeout
+    curr_time = time.time()
+    if curr_time-start_time >= TIME_OUT :
+        break
+    ready = select.select([sock], [], [], 1);
+    if not ready[0] :
+        continue
+
+    # receive messages
+    msg = sock.recvfrom(1024)
+
+    reply = re.match("^HTTP/[0-9]\.[0-9] 200.*", msg[0])
+    if not reply :
+        print "Not HTTP 200 OK"
+        print msg[0]
+        continue # not a successfull HTTP reply
+
+    if not re.search("ST: urn:schemas-avermedia-com:avercaster:1", msg[0]) :
         print "not AVerCaster"
         #print msg[0]
         continue # not a AVerCaster SSDP NOTIFY message, skip
 
-    #print msg[0]
-    print "NOTIFY from", msg[1][0]
-    #print msg[1][0] #ip address
-    
     device = { 'msg': '', 'ip1': '0.0.0.0', 'ip2': '0.0.0.0',
                'mac1': '', 'mac2': '', 'model': '',
                'version': ''}
@@ -55,11 +62,11 @@ while True:
 
     avercasters[msg[1][0]] = device; # insert by IP address
 
-    curr_time = time.time()
-    if curr_time-start_time > WORK_TIME :
-        break
+    #print "[Message]: "
+    #print msg[0]
 
 print "Collection done, start querying"
+
 #print avercasters
 for key in avercasters :
     #print avercasters[key]['ip1']
@@ -81,13 +88,28 @@ for key in avercasters :
     # parsing XML
     root = ET.fromstring(info)
     
-    product_tag = root.find('product_name')
-    avercasters[key]['model'] = product_tag.text
-    avercasters[key]['mac1'] = root.find('mac').text
-    avercasters[key]['mac2'] = root.find('mac2').text
-    avercasters[key]['version'] = root.find('fw_ver').text
+    try:
+        avercasters[key]['model'] = root.find('product_name').text
+    except AttributeError:
+        print "cannot find product_name tag"
+    
+    try:
+        avercasters[key]['mac1'] = root.find('mac').text
+    except AttributeError:
+        print "cannot find mac tag"
+
+    try:
+        avercasters[key]['mac2'] = root.find('mac2').text
+    except AttributeError:
+        print "cannot find mac2 tag"
+    
+    try:
+        avercasters[key]['version'] = root.find('fw_ver').text
+    except AttributeError:
+        print "cannot find fw_ver tag"
 
 
+print
 print "Found", len(avercasters), "products"
 for key in avercasters :
     print "Model:", avercasters[key]['model']
