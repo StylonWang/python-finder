db663739a91ef2075942a7a622bbd9c76ee38e36
Stylon Wang <wangstylon@gmail.com>
2015-11-12 17:05:12 +0800

    remove use of SO_REUSEPORT for compatibility outside Mac OS
    	modified:   avercaster-finder3.py

--- avercaster-finder3.py
+++ avercaster-finder3.py
@@ -15,18 +15,18 @@ import json
 MCAST_GRP = '234.8.8.8'
 MCAST_PORT = 8888
 MSG = "{\n\"Command\":\"browse\"\n}"
-TIME_OUT = 5
+TIME_OUT = 3
 
 # set up sender socket
 ssock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 ssock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 32)
 ssock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-ssock.bind( ("0.0.0.0", MCAST_PORT) )
+#ssock.bind( ("0.0.0.0", MCAST_PORT) )
 
 # set up receiver socket
 rsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 rsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-rsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+#rsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 rsock.setblocking(0)
 rsock.bind( (MCAST_GRP, MCAST_PORT) )
 
@@ -56,7 +56,8 @@ while True:
     # receive messages
     msg = rsock.recvfrom(1024)
 
-    print "[Message]: " , msg[1][0]
+    #print "[Message]: " , msg[1][0]
+    print "Searching..."
     #print msg[0]
 
     # parsing JSON 
